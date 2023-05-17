88abc169b554c52f42100a9915ec0bc202a7b968
Adriath <adrianarjonabravo@gmail.com>
2023-05-17 19:51:32 +0200

    Terminado método eventoDecimal().

--- src/interfaz/MarcoPrincipal.java
+++ src/interfaz/MarcoPrincipal.java
@@ -193,8 +193,29 @@ public class MarcoPrincipal extends JFrame{
             }
         }
         
-         private void eventoDecimal(){
+        
+        
+        /**
+         * Método privado que añade funcionalidad al botón "." de la calculadora.
+         */
+        private void eventoDecimal(){
             
+            botones[15].addActionListener(new ActionListener(){
+                
+                @Override
+                public void actionPerformed(ActionEvent e){
+                    // Si todavía no he añadido el punto decimal al número actual
+                    
+                    if (!puntoDecimal) {
+                        
+                        marcoSecundario.setText(marcoSecundario.getText() + textoBotones[15]) ;
+                        puntoDecimal = true ; // Ya no puedo añadir el punto decimal en este número
+                        nuevoNumero = false ; // Por si empiezo el número con punto decimal ( por ejemplo, .537)
+                    }
+                    
+                } // Cierra actionPerformed()
+                
+            }); // Cierra ActionListener()
         }
          
         private void eventosOperaciones(){
